49d4ebcae3673af98c0010cabb1e5c29dcde4e6e
yogo <lzjiang2008@gmail.com>
2012-10-19 15:23:46 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kiki::Application.config.secret_token = '06c1727df397556df72d4090295d78efdcddbb2f1e61cb50093a805445cece191cb0d96a30b6c100062656719a1da86c5f284aaee4bcdbf316c1d40696b4d6db'
